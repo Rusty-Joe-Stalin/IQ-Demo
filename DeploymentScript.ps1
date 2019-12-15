ec14c1c2f9e564725d4df4fab64f2c2415cf03b6
Rusty-Joe-Stalin <kylecherewyk@hotmail.com>
2019-12-15 15:56:45 -0600

    Add files via upload

---
+++ DeploymentScript.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param
+(
+ [Parameter(Mandatory = $true)]
+ [String]
+ $username,
+ [Parameter(Mandatory = $true)]
+ [String]
+ $password 
+)
+
+#Restore build packages 
+dotnet restore 
+
+#Test Solution 
+dotnet test
+
+#Publish the package
+dotnet publish .\DemoProject\DemoProject.csproj -c Release -f netcoreapp3.0
+
+#Prep Kudu zip deploy
+$apiUrl = "https://kyles-iq-demo.scm.azurewebsites.net/api/zipdeploy"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+$userAgent = "powershell/1.0"
+
+Compress-Archive -Path ".\DemoProject\bin\release\netcoreapp3.0\publish\*" -DestinationPath ".\DemoProject" -Force
+#Deploy packkge to azure
+Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile "./*.zip" -ContentType "multipart/form-data"
+
+#Open browser to ping endpoint
+Start-Process "https://kyles-iq-demo.azurewebsites.net/ping"
