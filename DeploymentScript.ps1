061cf66176c24676c6bf8f721ec6407214d1c739
Rusty-Joe-Stalin <kylecherewyk@hotmail.com>
2019-12-16 18:15:59 -0600

    update layout of deployment script

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -18,12 +18,14 @@ dotnet test
 #Publish the package
 dotnet publish .\DemoProject\DemoProject.csproj -c Release -f netcoreapp3.0
 
+#Create Zip file
+Compress-Archive -Path ".\DemoProject\bin\release\netcoreapp3.0\publish\*" -DestinationPath ".\DemoProject" -Force
+
 #Prep Kudu zip deploy
 $apiUrl = "https://kyles-iq-demo.scm.azurewebsites.net/api/zipdeploy"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
 $userAgent = "powershell/1.0"
 
-Compress-Archive -Path ".\DemoProject\bin\release\netcoreapp3.0\publish\*" -DestinationPath ".\DemoProject" -Force
 #Deploy packkge to azure
 Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile "./*.zip" -ContentType "multipart/form-data"
 
